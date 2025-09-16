820125e1aebfe0f6376b56afc0c23e10cb7b98a9
Ngordon68 <ngordon688@gmail.com>
2025-09-15 22:26:51 -0400

    [Clean] - Replace UIMain with Geometry reader for Favorites View

--- RandomAdventure/Views/FavoritesView.swift
+++ RandomAdventure/Views/FavoritesView.swift
@@ -17,37 +17,39 @@ struct FavoritesView: View {
             VStack {
                 
                 Text("Favorites")
-                ScrollView {
-                    ForEach(userFavorites) { favorite in
-                        
-                        Rectangle()
-                            .cornerRadius(15)
-                            .foregroundStyle(Color(.secondary))
-                            .frame(width: UIScreen.main.bounds.width * 0.93, height: UIScreen.main.bounds.width * 0.3)
-                            .overlay(alignment: .center) {
-                                VStack {
-                                    Text(favorite.title)
-                                    Text(favorite.subtitle)
-                                }
-                                .foregroundStyle(Color(.customComponent))
-                            }
-                            .overlay(alignment: .topTrailing) {
-                                Button(action: {
-                                    userFavorites.removeAll(where: { $0.title == favorite.title })
-                                }, label: {
-                                    Image(systemName: "heart.circle")
-                                        .foregroundStyle(Color.pink)
-                                        .font(.title)
-                                        .padding(3)
-                                })
-                            }
+                GeometryReader { proxy in
+                    let width = proxy.size.width
+                    ScrollView {
+                        ForEach(userFavorites) { favorite in
                             
-                        
+                            Rectangle()
+                                .cornerRadius(15)
+                                .foregroundStyle(Color(.secondary))
+                                .frame(width: width * 0.93, height: width * 0.3)
+                                .overlay(alignment: .center) {
+                                    VStack {
+                                        Text(favorite.title)
+                                        Text(favorite.subtitle)
+                                    }
+                                    .foregroundStyle(Color(.customComponent))
+                                }
+                                .overlay(alignment: .topTrailing) {
+                                    Button(action: {
+                                        userFavorites.removeAll(where: { $0.title == favorite.title })
+                                    }, label: {
+                                        Image(systemName: "heart.circle")
+                                            .foregroundStyle(Color.pink)
+                                            .font(.title)
+                                            .padding(3)
+                                    })
+                                }
+                            
+                        }
+                        .font(.largeTitle)
+                        .bold()
                     }
-                    .font(.largeTitle)
-                    .bold()
+                    .padding()
                 }
-                .padding()
             }
         }
     }
